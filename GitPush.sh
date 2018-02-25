756f0ceb6d40ba3eb75c2365edf2c7a9802cf54c
Edwin Mahecha <edwmapa@gmail.com>
2018-02-25 13:49:51 -0500

    Script test

--- GitPush.sh
+++ GitPush.sh
@@ -1,9 +1,7 @@
 #Simple script. Adds all, commits, and push it.
-MESSAGE
 echo "---Simple Commit---"
 sleep .2
-echo "> commit message> "
-read MESSAGE
-git add --all && git commit -m $MESSAGE
-
+echo "Commit message: "
+read -p '> ' MESSAGE
+git add --all && git commit -m "$MESSAGE"
 git push origin master || echo "Can't push to master, check manually"
