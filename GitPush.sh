ef395f2161d3a602e5177e78665b0f80a196484d
Edwin Mahecha <edwmapa@gmail.com>
2018-02-25 13:53:23 -0500

    remove extra input

--- GitPush.sh
+++ GitPush.sh
@@ -3,5 +3,6 @@ echo "---Simple Commit---"
 sleep .2
 echo "Commit message: "
 read -p '> ' MESSAGE
+
 git add --all && git commit -m "$MESSAGE"
 git push origin master || echo "Can't push to master, check manually"
